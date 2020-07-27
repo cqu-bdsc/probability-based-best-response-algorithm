bcc0288a687fc4b1b980c2853b7313efc45c1775
neardws <singleangel@vip.qq.com>
2020-07-27 11:24:17 +0800

    gps2xy finished

--- gps2xy.py
+++ gps2xy.py
@@ -11,19 +11,37 @@ import pandas as pd
 import utm
 from config import settings
 
-df = pd.read_csv(settings.csv_name)
 
-vehicle_id = df['id'].drop_duplicates()
-latitude = df['latitude']
-longitude = df['longitude']
+def renew_csv():
+    df = pd.read_csv(settings.csv_name)
+    vehicle_id = df['id']
+    time = df['time']
+    latitude = df['latitude']
+    longitude = df['longitude']
+    print("read csv success")
+    list_xy = list_latlon_to_xy(latitude, longitude)
+    if list_xy:
+        list_x = list_xy[0]
+        list_y = list_xy[1]
+        new_df = pd.DataFrame({'id': vehicle_id, 'time': time, 'x': list_x, 'y': list_y})
+        new_df.to_csv(settings.xy_csv_name, index=False)
 
-# for v_id in vehicle_id:
-#     print()
 
-print(latitude.head(3))
-print(longitude.head(3))
+def list_latlon_to_xy(list_latitude, list_longitude):
+    list_x = []
+    list_y = []
+    if len(list_latitude) == len(list_longitude):
+        length = len(list_latitude)
+        for i in range(length):
+            xy_coordinate = utm.from_latlon(list_latitude[i], list_longitude[i])
+            list_x.append(float(xy_coordinate[0]))
+            list_y.append(float(xy_coordinate[1]))
+            print(i)
+    if len(list_x) == len(list_y):
+        return list_x, list_y
+    else:
+        return None
 
 
-for lat in latitude:
-    for lon in longitude:
-        print(utm.from_latlon(float(lat), float(lon)))
+if __name__ == '__main__':
+    renew_csv()
