5147185a5619bbb0d3df4f2f0446bc4994acdbb8
neardws <singleangel@vip.qq.com>
2020-07-27 20:23:26 +0800

    change file structure

--- pbbra.py
+++ pbbra.py
@@ -3,14 +3,32 @@ author: neardws
 data: 7/27/2020
 
 Input:
-fog node location
+1. Experiment Itself
+time slots
+loop number
+fog node id
 
+2. Fog Node
+fog node location
+communication range
+
+3. Tasks
+task set (including the required transmission rate)
+
+4. Players
+probability of changing action
+
+5. Communication
+gaussian noise
+symbol error rate
+max power
+min transmission rate
+path loss
 
 Output:
-
+cycles
+strategy
 profits
-
-
 
 """
 
